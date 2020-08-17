a2f34534a70e90b8aabd42a45f7a27fd6ad8f8ec
deanchanter <37911234+deanchanter@users.noreply.github.com>
2020-08-17 09:44:24 -0400

    updatesI

--- WIPStudy.py
+++ WIPStudy.py
@@ -21,33 +21,77 @@ and Quequed work (item not done but in progress at the end of 100 days )
 import pandas as pd
 from getTask import getTasks
 
+def run_WIP_study(runs=100,days=100,limit_list=[3,5,7], bins=10, alpha=.6):
 
-
-runs = 5000
-days = 100
-#limit = 3
-
-
-limit_list= [3,5,7,10,25]
-#limit_list = [3,5]
-
-cycletimedf = pd.DataFrame()
-
-#repeatable results
-#np.random.seed(23)  #23,45,10 
-#ct95th
-for limit in limit_list:
-    ct85thlist = []
-    for run in range(runs):
-        finished_tasks, not_finished_tasks = getTasks(days,limit)
-        # Build a Cycle Time Slice (End Day - Start Day for each Item/Index)
-        finished_tasks["Cycle_Time"] = finished_tasks["day_ended"] - finished_tasks["day_started"] + 1
-        ct85th = int(finished_tasks["Cycle_Time"].quantile(.85))
-        ct85thlist.append(ct85th)
-    cycletimedf[limit] = ct85thlist
-
-cycletimedf.plot.hist(bins=50)
-                                          
+    #runs = 100000
+    #days = 100
+    #limit = 3
+    
+    
+    #limit_list= [3,5,7,10,25,101]
+    #limit_list = [3,5]
+    
+    cycletimedf = pd.DataFrame()
+    throughtputdf = pd.DataFrame()
+    notfinishedtotaldf = pd.DataFrame()
+    notfinishedagedf = pd.DataFrame()
+    
+    #repeatable results
+    #np.random.seed(23)  #23,45,10 
+    #ct95th
+    
+    for limit in limit_list:
+        print("Limit:",limit)
+        ct85thlist = []
+        tptlist = []
+        nftlisit = []
+        nfalisit = []
+        for run in range(runs):
+            #if run % 1000 == 0:
+                #print(run)
+            finished_tasks, not_finished_tasks = getTasks(days,limit)
+            # Build a Cycle Time Slice (End Day - Start Day for each Item/Index)
+            finished_tasks["Cycle_Time"] = finished_tasks["day_ended"] - finished_tasks["day_started"] + 1
+            ct85th = int(finished_tasks["Cycle_Time"].quantile(.85))
+            ct85thlist.append(ct85th)
+            
+            #Build a Throughput slice
+            tpt = finished_tasks["day_ended"].count()
+            tptlist.append(tpt)
+            
+            #Buidling a aging slice
+            if not_finished_tasks.empty:
+                nftlisit.append(0)
+                nfalisit.append(0)
+             
+            else:
+                not_finished_tasks.columns=["day_started"]
+                nftt = not_finished_tasks["day_started"].count()
+                nftlisit.append(nftt)
+            
+                not_finished_tasks["days_old"] = 100 -  not_finished_tasks["day_started"]
+                nfam = not_finished_tasks["days_old"].max()
+                nfalisit.append(nfam)
+           
+    
+                
+            
+            
+        cycletimedf[limit] = ct85thlist
+        throughtputdf[limit] = tptlist
+        notfinishedtotaldf[limit] = nftlisit
+        notfinishedagedf[limit] = nfalisit
+        
+    cycletimedf.to_csv('ct.csv')
+    throughtputdf.to_csv('tp.csv')
+    notfinishedagedf.to_csv('nf.csv')
+    notfinishedagedf.to_csv('age.csv')
+    
+    cycletimedf.plot.hist(bins=bins, alpha=alpha).set_title("Cycle Time")
+    throughtputdf.plot.hist(bins=bins, alpha=alpha).set_title("Throughput")
+    notfinishedtotaldf.plot.hist(bins=bins, alpha=alpha).set_title("# of WIP tasks ")
+    notfinishedagedf.plot.hist(bins=bins, alpha=alpha).set_title("max age of WIP")
+    return 0
 
 
 

08f97c1472094355472313e36eb7ce58488c364c
deanchanter <37911234+deanchanter@users.noreply.github.com>
2020-08-17 09:47:11 -0400

    updates

--- randomSample.py
+++ randomSample.py
@@ -7,19 +7,18 @@ At the end of 100 "days" it writes CSV. It also creates simple
 plot to show what a cycle time and throughput chart might look like.
 """
 
-import numpy as np
+
 import matplotlib.pyplot as plt
 import pandas as pd
-from getTask import getTasks
+import getTask as gt
 
 
-finished_tasks, not_finshed_tasks = getTasks(100,10)
-
+finished_tasks = gt.getTaskstoFinish(10, people=4, tasks=100)
 
 
 
 # dump to CSV
-finished_tasks.to_csv("randomSample")
+finished_tasks.to_csv("randomSample.csv")
 
 
 # Build a Cycle Time Slice (End Day - Start Day for each Item/Index)
@@ -40,29 +39,32 @@ throughput["Running Total Throuphput"] = throughput['Weekly Throughput'].cumsum(
 # plot for reference
 # Throughtput plot
 fig, axes = plt.subplots(2, 2)
-fig.tight_layout()
+#fig.subplots_adjust(wspace=1, hspace=1, left=0.125,right=0.9,top=2,bottom=0.9)
+fig.tight_layout(w_pad=1.5, h_pad=4.0)
+
 throughput["Weekly Throughput"].plot.bar(ax=axes[0,0])
+axes[0,0].set(title="Weekly Throughput", xlabel = "Time (Weeks)", ylabel="# of items completed")
 throughput["Running Total Throuphput"].plot(ax=axes[1,0])
+axes[1,0].set(title="Cumulative Throughtput",xlabel = "Time (Weeks)", ylabel="# of items completed")
 
 # Cycle Time Plot
 
 finished_tasks.plot.scatter(x="day_ended", y="Cycle_Time" ,ax=axes[0,1])
+axes[0,1].set(title="Cycle Time Scatter Plot",xlabel = "Time (Days)", ylabel="# of days in prgress")
 
 
-#make an aging slice
-
-not_finshed_tasks.columns=["day_started"]
-
-not_finshed_tasks["days_old"] = 100 -  not_finshed_tasks["day_started"]
-not_finshed_tasks.index.name='task#'
-print(not_finshed_tasks)
-
-lefts = not_finshed_tasks['day_started'].to_list()
-
-#lefts=[1,2,3]
-
-not_finshed_tasks["days_old"].plot.barh(ax=axes[1,1],legend=False, left=lefts)
-
+#make an aging slice and chart if there are leftover tasks
+'''
+if len(not_finshed_tasks.index) > 0: 
+    not_finshed_tasks.columns=["day_started"] 
+    not_finshed_tasks["days_old"] = 100 -  not_finshed_tasks["day_started"]
+    not_finshed_tasks.index.name='task#'
+    lefts = not_finshed_tasks['day_started'].to_list()
+    not_finshed_tasks["days_old"].plot.barh(ax=axes[1,1],legend=False, left=lefts)
+    
+    axes[1,1].set(title="Aging Plot",xlabel = "Time (Days)")
+    axes[1,1].axes.yaxis.set_visible(False)
+'''
 
 
 
